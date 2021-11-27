a0228fd14c4230b12a21c328feab233135cceef0
tahmid4tune <tahmid.tee10@gmail.com>
2021-11-27 15:18:29 +0600

    Dev push

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
   create(createUserInput: CreateUserInput) {
     return 'This action adds a new user';
   }
